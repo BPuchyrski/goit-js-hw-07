1a1cc9f7218c00fac8e535338a30110773c2bc93
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2022-12-15 16:37:53 +0100

    u

---
+++ js/01-gallery.js
@@ -0,0 +1,20 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const divGallery = document.querySelector(".gallery");
+divGallery.classList.add("gallery__item");
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const link = document.createElement("a");
+  const img = document.createElement("img");
+  divGallery.append(link);
+  link.append(img);
+  link.classList.add("gallery__link");
+  img.classList.add("gallery__image");
+  img.setAttribute("src", `${galleryItems[i].preview}`);
+  img.setAttribute("alt", `${galleryItems[i].description}`);
+  img.dataset.source = `${galleryItems[i].original}`;
+  link.setAttribute("href", `${galleryItems[i].original}`);
+}
